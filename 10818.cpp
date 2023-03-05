f20becec5c878fff85874b5b7eb821585132ee16
mnngy01 <78626832+mnngy01@users.noreply.github.com>
2023-03-06 00:09:49 +0900

    Add files via upload
    [백준] 10818 n개의 정수에서 최소, 최대 찾기

---
+++ 10818.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	int* number = new int[n]; //동적할당
+
+	for (int i = 0; i < n; i++)
+		cin >> number[i];
+
+	//최소값
+	int min = number[0];
+	for (int i = 0; i < n; i++)
+	{
+		if (number[i] < min)
+			min = number[i];
+	}
+
+	//최대값
+	int max = number[0];
+	for (int i = 0; i < n; i++)
+	{
+		if (number[i] > max)
+			max = number[i];
+	}
+	
+	cout << min << " " << max;
+	return 0;
+}
